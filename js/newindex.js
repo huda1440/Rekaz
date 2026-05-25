3caf6e384bd8b2d0f9b9b3f5a162fbe07127f112
Huda Alandijani <47171765+huda1440@users.noreply.github.com>
2026-05-25 17:37:22 +0300

    Update newindex.js

--- js/newindex.js
+++ js/newindex.js
@@ -14,7 +14,7 @@ var ctx1 = canvas1.getContext('2d');
 var img1 = new Image();
 img1.crossOrigin="anonymous";
 
-window.addEventListener('load', DrawPlaceholder(img1,ctx1,"#308F98",'p2.jpg',345,400));
+window.addEventListener('load', DrawPlaceholder(img1,ctx1,"#308F98",'p2.jpg',345,600));
 
 
 var canvas2 = document.getElementById('imageCanvas2');
